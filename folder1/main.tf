4e2b358087010f350ebea25155151c50eda6dab7
Coder63 <pbharath63@gmail.com>
2017-07-11 15:11:09 -0700

    Update main.tf

--- folder1/main.tf
+++ folder1/main.tf
@@ -16,12 +16,10 @@ module "Security_Group"  {
   source = "./Security_Group"
   ip_range = "${var.ip_range}"
 }
-
 module "CoreInfra" {
   source = "./Instance/CoreInfra"
   ssh_http_sg_id = "${module.Security_Group.ssh_http_sg_id}"
 }
-
 module "FSS" {
   source = "./Instance/FSS"
   ssh_http_sg_id = "${module.Security_Group.ssh_http_sg_id}"
