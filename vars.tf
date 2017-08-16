48780e64a17c0417ee95ab2ad075147580202db2
Coder63 <pbharath63@gmail.com>
2017-08-16 15:12:41 -0700

    Update vars.tf

--- vars.tf
+++ vars.tf
@@ -2,14 +2,6 @@ variable "region" {
   default = "us-west-2"
 }
 
-variable "aws_accesskey"{
-  default =  aws_access_key
-  }
-
-variable "aws_secret_accesskey"{
-  default = aws_secret_access_key
-  }
-
 variable "vpc_cidr" {
   description = "CIDR for the whole VPC"
   default = "172.0.0.0/16"
